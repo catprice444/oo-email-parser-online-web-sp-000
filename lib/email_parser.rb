ead11ee41c4b331e18a11c26e48e819d051b1db9
Catherine Price <catprice444@gmail.com>
2020-05-23 18:12:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,9 +11,10 @@ class EmailParser
   end
 
   def parse
-   email_array = @email.split(/[,]/).uniq
-   email_array.reject! {|element| element.empty?}
-   email_array
+    emails.delete(',').split.uniq
+  #  email_array = @email.split(/[,]/).uniq
+  #  email_array.reject! {|element| element.empty?}
+  #  email_array
   end
 
 
